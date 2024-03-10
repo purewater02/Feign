db0303873db9df22e7dde1e1914008853885945c
purewater02 <ksmin02@gmail.com>
2024-03-10 21:15:12 +0900

    feature: logback config added

--- src/main/java/com/example/feign/FeignApplication.java
+++ src/main/java/com/example/feign/FeignApplication.java
@@ -1,9 +1,11 @@
 package com.example.feign;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@Slf4j
 @EnableFeignClients
 @SpringBootApplication
 public class FeignApplication {
--- src/main/java/com/example/feign/config/FeignConfig.java
+++ src/main/java/com/example/feign/config/FeignConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignConfig {
   // Global FeignConfig
+
   @Bean
   public Logger feignLogger() {
     return new FeignCustomLogger();
